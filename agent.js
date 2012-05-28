6b8e4c2d79a902f37ea2bfc0080e06eb11d244ab
Atom Mac <=>
2012-05-28 15:49:45 -0400

    -Tweaked some parameters to suit desired agent behavior. -Added really hackish code to limit distance between agent and random target.

--- agent.js
+++ agent.js
@@ -3,7 +3,8 @@ PITRI.Agent = function(config)
 	var defaults = {
 		name: "Agent" + Math.random(),
 		brain: new PITRI.Wanderer({agent:this}),
-		body: new PITRI.PointMassModel({agent:this})
+		body: new PITRI.PointMassModel({agent:this}),
+		skin: new PITRI.GenericAgentSkin()
 	}
 	
 	// Internal reference to self.
@@ -34,7 +35,7 @@ PITRI.PointMassModel = function(config)
 {
 	var defaults = {
 		maxSpeed: 5,
-		maxAccel: 1,
+		maxAccel: 5,
 		position: new Vector(0,0),
 		velocity: new Vector(0,0),
 		accel: new Vector(0,0),
@@ -61,10 +62,12 @@ PITRI.PointMassModel = function(config)
 		var steer = config.agent.state.brain.state.steer;
 		
 		// Calculate overall force.
+		/*
 		me.state.inertia.x = (me.state.mass * me.state.velocity.x);
 		me.state.inertia.y = (me.state.mass * me.state.velocity.y);
 		me.state.inertia.normalize();
 		steer.add(me.state.inertia);
+		*/
 		
 		// Calculate acceleration and truncate to maxAccel.
 		me.state.accel.x = (steer.x / me.state.mass, steer.x / me.state.mass);
@@ -77,18 +80,6 @@ PITRI.PointMassModel = function(config)
 		
 		// Move me!
 		me.state.position = me.state.position.add(me.state.velocity);
-		
-		// DEBUG CODE.
-		/*
-		console.log("Steer: ");
-		console.log(steer);
-		console.log("Accel: ");
-		console.log(me.state.accel);
-		console.log("Velocity: ");
-		console.log(me.state.velocity);
-		console.log("Position: ");
-		console.log(me.state.position);
-		*/
 	}
 	
 	// Initialize.
@@ -99,7 +90,9 @@ PITRI.Wanderer = function(config)
 {
 	// Defaults
 	var defaults = {
-		steer: new Vector(0,0)
+		steer: new Vector(0,0),
+		targetDist: 40,
+		maxDist: 80
 	}
 	
 	// Internal reference to self.
@@ -124,7 +117,7 @@ PITRI.Wanderer = function(config)
 		// Do I need to choose a new target yet?
 		var distance = me.state.target.sub(body.state.position);
 		distance = distance.len();
-		if(distance < 20) {
+		if(distance < me.state.targetDist) {
 			me.state.target = me.getNewTarget();
 		}
 	
@@ -136,12 +129,51 @@ PITRI.Wanderer = function(config)
 	// Return a random target to move toward.
 	me.getNewTarget = function() 
 	{	
-		var x = Math.randInt(0, 400);
-		var y = Math.randInt(0, 400);
+		/*
+		 * OK... This is an incredibly bad implementation for limiting the
+		 * distance that the generated target will be from the Agent but I'm
+		 * really stoned and I just wanted to see what it looks like.  I'll fix
+		 * this later, I swear.
+		 */
+		var distance = 80;
+		try{
+			var body = config.agent.state.body;
+		} catch(err){
+			minx = 0;
+			maxx = 400;
+			miny = 0;
+			maxy = 400;
+			var x = Math.randInt(minx, maxx);
+			var y = Math.randInt(miny, maxy);
+			
+			return new Vector(x,y);
+		}
+		
+		minx = body.state.position.x - distance;
+		maxx = body.state.position.x + distance;
+		if(minx < 0) minx = 0;
+		if(maxx > 400) maxx = 400;
+		
+		miny = body.state.position.y - distance;
+		maxy = body.state.position.y + distance;
+		if(miny < 0) miny = 0;
+		if(maxy > 400) maxy = 400;
+		// End amazingly hack-ish code.
+		
+		var x = Math.randInt(minx, maxx);
+		var y = Math.randInt(miny, maxy);
 		
 		return new Vector(x,y);
 	}
 	
 	// Initialize.
 	me.init();
+}
+
+PITRI.GenericAgentSkin = function(config) {
+	// Defaults
+	var defaults = {
+		color: "rgb(0, 255, 0)",
+		size: 10
+	}
 }
